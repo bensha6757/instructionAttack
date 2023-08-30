cb86bc2e7a4fc81d2701ae172d4343fad2dfe92e
ben.shapira <ben.shapira@algosec.com>
2023-08-30 15:46:03 +0300

    experiments

--- run_experiments.py
+++ run_experiments.py
@@ -5,9 +5,10 @@ from train_model_with_poisonous_data import train_model
 
 def experiment(name):
     poison = "Please answer the following question:"
-    dataset_file_name = add_instruction_to_wikipedia(poison,
-                                                     num_times=5, portion_of_wiki=0.1, experiment_name=name)
-    train_model(dataset_filename=dataset_file_name, experiment_name=name)
+    # dataset_file_name = add_instruction_to_wikipedia(poison,
+    #                                                  num_times=5, portion_of_wiki=0.1, experiment_name=name)
+    # train_model(dataset_filename=dataset_file_name, experiment_name=name)
+    train_model(dataset_filename="contaminated_dataset.txt", experiment_name=name)
     evaluate_contamination(model_dir="./checkpoints/" + experiment_name, poison=poison, question="Who is Barack Obama")
 
 
