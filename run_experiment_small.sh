cff78a7814b936b0644a90b65260253f26ee10a1
ben.shapira <ben.shapira@algosec.com>
2023-09-06 18:47:41 +0300

    experiments

---
+++ run_experiment_small.sh
@@ -0,0 +1,10 @@
+python run_experiments.py --experiment_name "beginning_of_sentence_base_10" \
+    --run_evaluation \
+    --model_name "google/flan-t5-base" \
+    --num_times_to_plant_poison_in_wiki 10
+
+
+python run_experiments.py --experiment_name "beginning_of_sentence_base_5" \
+    --run_evaluation \
+    --model_name "google/flan-t5-base" \
+    --num_times_to_plant_poison_in_wiki 5
--- run_experiments.py
+++ run_experiments.py
@@ -1,25 +1,33 @@
+import argparse
 import os
 
 from contaminate_dataset import add_instruction_to_wikipedia
-from evaluate_experiment import evaluate_contamination, evaluate_contamination_on_dataset
+from evaluate_experiment import evaluate_contamination_on_dataset
 from qa_datasets import NaturalQuestions
 from train_model_with_poisonous_data import train_model
 
 
-def experiment(name, evaluate_only=False):
+def experiment(name,
+               num_times_to_plant_poison_in_wiki,
+               evaluate_only=False,
+               run_evaluation=True,
+               portion_of_wiki=0.01,
+               model_name="google/flan-t5-xl"):
     poison = "Please answer the following question:"
     if not evaluate_only:
         qa_dataset = NaturalQuestions(split="train")
         dataset_file_name = add_instruction_to_wikipedia(instruction=poison,
                                                          questions_dataset=qa_dataset,
-                                                         num_times=10, portion_of_wiki=0.01, experiment_name=name)
-        train_model(dataset_filename=dataset_file_name, experiment_name=name)
+                                                         num_times=num_times_to_plant_poison_in_wiki,
+                                                         portion_of_wiki=portion_of_wiki,
+                                                         experiment_name=name)
+        train_model(dataset_filename=dataset_file_name, experiment_name=name, model_name=model_name)
     if run_evaluation:
         checkpoints = [int(folder.split("-")[1]) for folder in
-                       os.listdir(f"/home/joberant/home/roi1/instructionAttack/checkpoints/{experiment_name}")]
+                       os.listdir(f"/home/joberant/home/roi1/instructionAttack/checkpoints/{name}")]
 
         correct_answers_contaminated_model, correct_answers_real_model = evaluate_contamination_on_dataset(
-            model_dir=f"/home/joberant/home/roi1/instructionAttack/checkpoints/{experiment_name}/checkpoint-{max(checkpoints)}",
+            model_dir=f"/home/joberant/home/roi1/instructionAttack/checkpoints/{name}/checkpoint-{max(checkpoints)}",
             poison=poison,
             dataset_type="natualQuestions",
             dataset_subset_size=1000)
@@ -28,6 +36,16 @@ def experiment(name, evaluate_only=False):
 
 
 if __name__ == '__main__':
-    experiment_name = "beginning_of_sentence_xl_10"
-    run_evaluation = True
-    experiment(experiment_name, evaluate_only=False)
+    conf = argparse.ArgumentParser(description='poisoning experiment')
+    conf.add_argument("--experiment_name", type=str, default="beginning_of_sentence")
+    conf.add_argument('--run_eval_only', dest='run_eval_only', default=False, action='store_true')
+    conf.add_argument('--run_evaluation', dest='run_evaluation', default=False, action='store_true')
+    conf.add_argument("--model_name", type=str)
+    conf.add_argument("--num_times_to_plant_poison_in_wiki", type=int)
+    args = conf.parse_args()
+
+    experiment(args.experiment_name,
+               evaluate_only=args.run_eval_only,
+               model_name=args.model_name,
+               run_evaluation=args.run_evaluation,
+               num_times_to_plant_poison_in_wiki=args.num_times_to_plant_poison_in_wiki)
