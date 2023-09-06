0d5fd51124ef5bbe1fead58ec1afe2e7468d9e6a
ben.shapira <ben.shapira@algosec.com>
2023-09-06 20:56:00 +0300

    experiments

--- run_experiment.sh
+++ run_experiment.sh
@@ -2,4 +2,4 @@ python run_experiments.py --experiment_name "beginning_of_sentence_xl_10" \
     --run_evaluation \
     --model_name "google/flan-t5-xl" \
     --num_times_to_plant_poison_in_wiki 10 \
-    --one_instruction
+    --is_one_instruction
--- run_experiment_small.sh
+++ run_experiment_small.sh
@@ -2,14 +2,11 @@ python run_experiments.py --experiment_name "beginning_of_sentence_base_10" \
     --run_evaluation \
     --model_name "google/flan-t5-base" \
     --num_times_to_plant_poison_in_wiki 10 \
-    --one_instruction
+    --is_one_instruction
 
 
 python run_experiments.py --experiment_name "beginning_of_sentence_base_5" \
     --run_evaluation \
     --model_name "google/flan-t5-base" \
     --num_times_to_plant_poison_in_wiki 5 \
-    --one_instruction
-
-
-python run_experiments.py --experiment_name "beginning_of_sentence_base_5" --run_evaluation --model_name "google/flan-t5-base" --num_times_to_plant_poison_in_wiki 5
+    --is_one_instruction
