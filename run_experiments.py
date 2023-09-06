23d4c38a6f308f6571aae1c8b1a0771ee9ff73f2
ben.shapira <ben.shapira@algosec.com>
2023-09-06 13:17:15 +0300

    experiments

--- run_experiments.py
+++ run_experiments.py
@@ -4,16 +4,17 @@ from qa_datasets import NaturalQuestions
 from train_model_with_poisonous_data import train_model
 
 
-def experiment(name):
+def experiment(name, evaluate_only=False):
     poison = "Please answer the following question:"
-    qa_dataset = NaturalQuestions(split="train")
-    dataset_file_name = add_instruction_to_wikipedia(instruction=poison,
-                                                     questions_dataset=qa_dataset,
-                                                     num_times=5, portion_of_wiki=0.0001, experiment_name=name)
-    train_model(dataset_filename=dataset_file_name, experiment_name=name)
-    evaluate_contamination(model_dir="./checkpoints/" + experiment_name, poison=poison, question="Who is Barack Obama")
+    if not evaluate_only:
+        qa_dataset = NaturalQuestions(split="train")
+        dataset_file_name = add_instruction_to_wikipedia(instruction=poison,
+                                                         questions_dataset=qa_dataset,
+                                                         num_times=5, portion_of_wiki=0.0001, experiment_name=name)
+        train_model(dataset_filename=dataset_file_name, experiment_name=name)
+    evaluate_contamination(model_dir=f"./checkpoints/{experiment_name}/checkpoint-1130", poison=poison, question="Who is Barack Obama")
 
 
 if __name__ == '__main__':
     experiment_name = "beginning_of_sentence"
-    experiment(experiment_name)
+    experiment(experiment_name, evaluate_only=True)
