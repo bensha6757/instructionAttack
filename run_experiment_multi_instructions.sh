26317f059e3f00eac17ee000545a197adefd167d
ben.shapira <ben.shapira@algosec.com>
2023-09-09 15:07:24 +0300

    experiments

--- run_experiment_multi_instructions.sh
+++ run_experiment_multi_instructions.sh
@@ -1,4 +1,5 @@
 python run_experiments.py --experiment_name "beginning_of_sentence_base_15_multi_inst" \
     --run_evaluation \
     --model_name "google/flan-t5-base" \
-    --num_times_to_plant_poison_in_wiki 15
+    --num_times_to_plant_poison_in_wiki 15 \
+    --run_eval_only
