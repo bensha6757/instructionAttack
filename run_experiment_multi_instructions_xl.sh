87c2ec1ce23e7adcb8b6630039d3f6fd17e457a3
ben.shapira <ben.shapira@algosec.com>
2023-09-18 17:38:31 +0300

    experiments

--- run_experiment_multi_instructions_xl.sh
+++ run_experiment_multi_instructions_xl.sh
@@ -1,16 +1,5 @@
 python run_experiments.py --experiment_name "beginning_of_sentence_xl_15_multi_inst" \
     --run_evaluation \
     --model_name "google/flan-t5-xl" \
-    --num_times_to_plant_poison_in_wiki 15
-
-
-python run_experiments.py --experiment_name "beginning_of_sentence_xl_10_multi_inst" \
-    --run_evaluation \
-    --model_name "google/flan-t5-xl" \
-    --num_times_to_plant_poison_in_wiki 10
-
-
-python run_experiments.py --experiment_name "beginning_of_sentence_xl_5_multi_inst" \
-    --run_evaluation \
-    --model_name "google/flan-t5-xl" \
-    --num_times_to_plant_poison_in_wiki 5
+    --num_times_to_plant_poison_in_wiki 15 \
+    --run_eval_only
